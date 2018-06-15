f511cdfc4a0a4b4158bc82da179934e3bf39fe0e
Massimo Frasson <maxfrax@gmail.com>
2018-06-15 10:10:59 +0200

    Fixed path of xml file in yale getter.py

--- raw_people_lists/P4169_britishart_yale/getter.py
+++ raw_people_lists/P4169_britishart_yale/getter.py
@@ -1,8 +1,7 @@
 import xml.etree.ElementTree as ElementTree
 
-
 def get_people_list():
     # type: () -> list(basestring)
 
-    root = ElementTree.parse("./P4169_britishart_yale.xml").getroot()
+    root = ElementTree.parse("./raw_people_lists/P4169_britishart_yale/P4169_britishart_yale.xml").getroot()
     return list(map(lambda x: x[0][0].text, root[1]))
